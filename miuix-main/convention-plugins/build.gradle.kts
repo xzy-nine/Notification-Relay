66045c1d6ae320d0c02c25331ee42b8a2d7dd132
xzyht-nine <xzyht-nine@outlook.com>
2025-12-09 23:36:46 +0800

    移除本仓库内的miuix仓库内容

--- miuix-main/convention-plugins/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-// Copyright 2025, miuix-kotlin-multiplatform contributors
-// SPDX-License-Identifier: Apache-2.0
-
-plugins {
-    `kotlin-dsl`
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,4 +20,4 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "NotifyRelay"
-include(":app")
+include(":app")
